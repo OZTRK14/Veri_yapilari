941528f3edfc0abe0b47a261bfaf801d9d5b88c7
OZTRK14 <omerfarukozturk91@gmail.com>
2023-01-07 20:29:09 +0300

    test

---
+++ Veri Yapıları/Veri Yapıları/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veri_Yapıları
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}

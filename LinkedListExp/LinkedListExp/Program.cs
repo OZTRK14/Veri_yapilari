e7583d1a9d55ededa80a8f6c54ba41e010578fd8
OZTRK14 <omerfarukozturk91@gmail.com>
2023-02-21 14:18:01 +0300

    güncel

---
+++ LinkedListExp/LinkedListExp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExp
+{
+    internal class LinkedListSoru
+    {
+       
+        internal class Program
+        {
+            class Block
+            {
+                public int data;
+                public Block next;
+                public Block prev;
+            }
+            static void Main(string[] args)
+            {
+                Block head = null;
+                Block last = null;
+                for (int i = 0; i < 10; i++)
+                {
+                    Block temp = new Block();
+                    temp.data = i;
+                    temp.next = null;
+                    if(head==null)
+                    {
+                        head = temp;
+                        last = temp;
+                    }
+                    else
+                    {
+                        last.next = temp;
+                        temp.prev = last;
+                        last = temp;
+                    }
+
+
+                }
+              
+            }
+          
+           
+        }
+
+    }
+}
